18ab5dde48507ed47fa5da9bcf35bc198ec6afe4
RaphaelBensTAU <73940756+RaphaelBensTAU@users.noreply.github.com>
2021-08-14 18:44:34 +0300

    Update onto_to_format2.py

--- scripts/format2/onto_to_format2.py
+++ scripts/format2/onto_to_format2.py
@@ -1,5 +1,7 @@
 import sys
+import json
 from scripts.format1 import format1_to_clusters, onto_to_format1
+import os
 
 def flatten_list_of_lists(lst):
     return [elem for sublst in lst for elem in sublst]
@@ -11,22 +13,25 @@ def convert_file(file_name):
         tmp_name = '../../tmp_file'
         onto_to_format1.write_results(file_name, tmp_name)
         format1_to_cluster = format1_to_clusters.convert_file(tmp_name)
+        os.remove(tmp_name)
         for i, doc in enumerate(format1_to_cluster):
+            json = {}
             to_write_doc = ''
             for cluster in doc['clusters']:
                 for x,y in cluster:
                     to_write_doc += str([x,y]) + ' '
                 to_write_doc += 'SEP '
-            to_write_doc = to_write_doc[:-5]
-            result.append((" ".join(flatten_list_of_lists(doc['sentences'])),to_write_doc))
+            json['sentences'] = doc['sentences']
+            json['target'] = to_write_doc[:-5]
+            result.append(json)
     return result
 
 def write_results(to_convert, to_output):
     result = convert_file(to_convert)
-    with open('{}'.format(to_output), 'w+') as f:
-        for elem in result:
-            to_write = elem[0]+'\t'+elem[1]+'\n'
-            f.write(to_write)
+    with open(to_output, "w") as outfile:
+        for i in result:
+            json.dump(i, outfile)
+            outfile.write('\n')
 
 if __name__ == '__main__':
     #INPUT FILE - original data file i.e., train/test/dev
